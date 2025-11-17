d5fb50d0bb38f83f83f32caeea2c0aa3dea3e8cc
Almedina Hadrovic <almedinahadrovic5@gmail.com>
2025-11-17 11:38:32 +0100

    Dodana lekcija 3 - aritmeticke operacije

---
+++ src/Lekcija3.java
@@ -0,0 +1,13 @@
+public class Lekcija3 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 3;
+
+        System.out.println("Sabiranje: " + (a + b));
+        System.out.println("Oduzimanje: " + (a - b));
+        System.out.println("Mnozenje: " + (a * b));
+        System.out.println("Dijeljenje: " + (a / b));     // integer dijeljenje
+        System.out.println("Modulus (ostatak): " + (a % b));
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+    // Lekcija 2: Promjenljive (variables) i tipovi podataka u Javi
+    public static void main(String[] args) {
+
+        // 1. Tekst (String)
+        String country = "Austria";
+        String city = "Wels";
+
+        // 2. Cijeli brojevi (int, long)
+        int population = 62000;      // broj stanovnika
+        long worldPopulation = 8000000000L; // globalna populacija
+
+        // 3. Realni brojevi (double, float)
+        double temperature = 18.5;   // trenutna temperatura u °C
+
+        // 4. Logicka vrijednost (boolean)
+        boolean isStudent = true;
+        boolean hasJob = false;
+
+        // Ispis na ekranu
+        System.out.println("DRZAVA: " + country);
+        System.out.println("GRAD: " + city);
+        System.out.println("Broj stanovnika: " + population);
+        System.out.println("Temperatura: " + temperature + " °C");
+        System.out.println("Da li sam student? " + isStudent);
+        System.out.println("Da li trenutno radim? " + hasJob);
+        System.out.println("Svjetska populacija: " + worldPopulation);
+    }
 }
